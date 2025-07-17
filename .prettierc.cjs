2f4ec9db8163f07fbbd3b954d409b798276919f0
jun-hwan00 <0528klkl@naver.com>
2025-07-17 17:08:24 +0900

    Chore: add settings
    ESLint, Prettier, TanStack Query 설정 추가 및 Axios 인스턴스 설정 추가

---
+++ .prettierc.cjs
@@ -0,0 +1,22 @@
+// .prettierrc.cjs
+
+module.exports = {
+  
+  // 탭 너비를 2칸으로 설정
+  tabWidth: 2,
+
+  // 문장 끝에 세미콜론을 추가
+  semi: true,
+  
+  // 문자열에 큰따옴표 대신 작은따옴표를 사용
+  singleQuote: true,
+  
+  // 여러 줄의 객체나 배열 끝에 항상 쉼표를 추가
+  trailingComma: 'es5',
+
+  // 객체 리터럴에서 괄호 양쪽의 공백을 추가
+  bracketSpacing: true,
+
+  // 화살표 함수에서 파라미터가 하나일 때도 괄호를 항상 추가
+  arrowParens: 'always',
+};
---
+++ eslint.config.js
@@ -0,0 +1,42 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import reactHooks from 'eslint-plugin-react-hooks'
+import reactRefresh from 'eslint-plugin-react-refresh'
+import tseslint from 'typescript-eslint'
+import { globalIgnores } from 'eslint/config'
+
+export default tseslint.config([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      reactHooks.configs['recommended-latest'],
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      '@typescript-eslint/naming-convention': [
+        'error',
+        // 1. 일반 변수, 함수, 객체는 camelCase
+        {
+          selector: 'default',
+          format: ['camelCase','PascalCase'],
+          leadingUnderscore: 'allow',
+        },
+      
+      // 3. 전역 상수는 UPPER_CASE 추가 허용
+        {
+          selector: 'variable',
+          modifiers: ['const', 'global'],
+          format: ['camelCase', 'UPPER_CASE'],
+        },
+        
+       ]
+    }
+  },
+])
