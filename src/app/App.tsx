96b487dbbc8df1737b9989f3f71b2181bc74b682
jun-hwan00 <123939697+jun-hwan00@users.noreply.github.com>
2025-07-20 19:50:11 +0900

    feat: font and color configuration (#4)
    디자인 시스템 폰트, 색상 설정
    
    Github issue #3

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,7 +5,11 @@ function App() {
   const element = useRoutes(routes);
   return (
     <div>
+<<<<<<< HEAD
+      <p className="sub-01 text-point">폰트 Test</p>
+=======
       {element}
+>>>>>>> e8d653557c228198eab0f83a9beee48053b998d7
     </div>
   );
 }
