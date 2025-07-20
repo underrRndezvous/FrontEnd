e8d653557c228198eab0f83a9beee48053b998d7
하주영 <92720304+hazzuu123@users.noreply.github.com>
2025-07-20 15:49:38 +0900

    Feat: 필요한 라이브러리 세팅 (#2)
    * feat: 라우팅 설정
    
    - react-router-dom 설치
    - strict mode 주석처리
    
    * chore: zustand 상태 라이브러리 설치
    
    * refactor: 라우팅 파일명 변경
    
    ---------
    
    Co-authored-by: jun-hwan00 <0528klkl@naver.com>
    Co-authored-by: hajuyeong <hajuyeong@hajuyeongui-MacBookPro.local>

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,11 @@
+import { BrowserRouter, useRoutes } from "react-router-dom";
+import routes from "./routes";
+
 function App() {
+  const element = useRoutes(routes);
   return (
     <div>
-      <p>시작</p>
+      {element}
     </div>
   );
 }
