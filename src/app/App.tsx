e4aeb76eed7691804e18cfb30d93ecbc80ee9554
jun-hwan00 <123939697+jun-hwan00@users.noreply.github.com>
2025-07-22 11:21:44 +0900

    Feat/add start page-step0 (#6)
    * feat: font and color configuration
    
    디자인 시스템 폰트, 색상 설정
    
    Github issue #3
    
    * feat: add start page-step0
    
    button1 공통 컴포넌트 생성
    meetingStart 위젯 생성 (Button1, plaza이미지, 텍스트 조합)
    step0 페이지 생성 및 라우팅 설정
    
    Github issue #5

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -3,15 +3,7 @@ import routes from "./routes";
 
 function App() {
   const element = useRoutes(routes);
-  return (
-    <div>
-<<<<<<< HEAD
-      <p className="sub-01 text-point">폰트 Test</p>
-=======
-      {element}
->>>>>>> e8d653557c228198eab0f83a9beee48053b998d7
-    </div>
-  );
+  return <div>{element}</div>;
 }
 
 export default App;
---
+++ src/pages/step0.tsx
@@ -0,0 +1,11 @@
+import MeetingStart from "@/widgets/meeting/meetingStart";
+
+const StartPage = () => {
+  return (
+    <main className="h-screen w-full">
+      <MeetingStart />
+    </main>
+  );
+};
+
+export default StartPage;
